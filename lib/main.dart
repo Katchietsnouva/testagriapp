d09da2dc1beab3f1aece843e125714b838893151
Katchietsnouva <philipaswa01@gmail.com>
2023-10-19 22:35:11 +0300

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:testagriapp/firebase_options.dart';
+
 import 'package:flutter/material.dart';
 import 'package:testagriapp/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,13 +19,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData( 
+      theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen (),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
