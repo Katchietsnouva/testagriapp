bbef1196d14101c248141e0363859f90c7453d0d
Katchietsnouva <philipaswa01@gmail.com>
2023-10-19 21:41:05 +0300

    q

---
+++ lib/home_screen.dart
@@ -0,0 +1,21 @@
+// ignore: unused_import
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AgriTransformers App '),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
